fb6d0e3ebf4744cf545ed33b185582430e38742d
Sameer Deshmukh <sameer.p.deshmukh@gmail.com>
2025-11-04 12:21:53 +0100

    Understanding of type and id keywords

---
+++ type_id.py
@@ -0,0 +1,22 @@
+# Printf combination of strings
+print("Hellow World", "Sameer Deshmukh")
+# Using escape sequences \
+print("Line 1\"Line2")
+print()
+
+a = 10
+print("The type of a is:", type(a))     # <class 'int'>
+b = 5.5
+print("The type of b is:", type(b))     # <class 'float'>
+c= "Sameer"
+print("The type of c is:", type(c))     # <class 'str'>
+d= True
+print("Type of d is :", type(d))        # <class 'bool'>
+e = complex(2,3)
+print("Type of e is :", type(e))        # <class 'complex'>
+
+print("The id of a is:", id(a))
+print("The id of b is:", id(b))
+print("The id of c is:", id(c))
+print("The id of d is:", id(d))
+print("The id of e is:", id(e))
